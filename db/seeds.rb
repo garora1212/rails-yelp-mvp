955a3202bca008f41ff50fb6ad44d56caa99b4e5
Gaurav Arora <gauravarora.sra@gmail.com>
2021-01-14 20:28:00 +0100

    rails yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+restaurant1 = Restaurant.create!(name: "Nirvana", address: "paris", category: "japanese" )
+restaurant2 = Restaurant.create!(name: "Little Italy", address: "madrid", category: "chinese" )
+restaurant3 = Restaurant.create!(name: "la fiche", address: "new york", category: "french" )
+restaurant4 = Restaurant.create!(name: "brandybu", address: "delhi", category: "belgian" )
+restaurant5 = Restaurant.create!(name: "pink mamma", address: "paris", category: "italian" )
